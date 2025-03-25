76a6aad3421d3fafe452f33a4b186ee148638c3b
Jordi Casanella <jordikiter@hotmail.es>
2025-03-25 00:42:11 +0000

    Add handler

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,16 @@ import (
 	"log"
 
 	"github.com/jcasanella/golang_chat/db"
+	"github.com/jcasanella/golang_chat/internal/user"
 )
 
 func main() {
-  _, err := db.NewDatabase()
-  if err != nil {
-    log.Fatalf("Could not initialize Database connection: %s", err)
-  }
+	dbConn, err := db.NewDatabase()
+	if err != nil {
+		log.Fatalf("Could not initialize Database connection: %s", err)
+	}
 
+	userRep := user.NewRepository(dbConn.GetDB())
+	userService := user.NewService(userRep)
+	userHandler := user.NewHandler(userService)
 }
