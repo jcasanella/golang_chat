0d8c03e0d845f26da4655bc2994bfeab810a68d2
Jordi Casanella <jordikiter@hotmail.es>
2025-03-25 00:06:37 +0000

    Handler using gin

--- internal/user/user_handler.go
+++ internal/user/user_handler.go
@@ -1,5 +1,33 @@
 package user
 
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
 
-funct NewHandler(service Service)*Handler {}
+type Handler struct {
+	Service
+}
+
+func NewHandler(service Service) *Handler {
+	return &Handler{
+		Service: service,
+	}
+}
+
+func (h *Handler) CreateUser(c *gin.Context) {
+	var u CreateUserReq
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := h.Service.CreateUser(c.Request.Context(), &u)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+}
