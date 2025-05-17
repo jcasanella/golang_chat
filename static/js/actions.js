9e467a38d07ab9ed55b455a0758cd80932b89288
Jordi Casanella <jordikiter@hotmail.es>
2025-05-17 11:04:17 +0100

    Remove not required styles and add js to action buttons

---
+++ static/js/actions.js
@@ -0,0 +1,29 @@
+const formSignUpElement = document.querySelector('.form #signup');
+const formLoginElement = document.querySelector('.form #login');
+
+const buttonSignUpElement = formSignUpElement.querySelector('button');
+const buttonLoginElement = formLoginElement.querySelector('button');
+
+buttonSignUpElement.addEventListener('click',async (_event) =>{
+    const resp = await fetch('/api/signup', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: formSignUpElement.querySelector('#username-input').value,
+            email: formSignUpElement.querySelector('#email-input').value,
+            password: formSignUpElement.querySelector('#password-input').value,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (!resp.ok) {
+        const errorMessageElement = formSignUpElement.querySelector('#error-message');
+        errorMessageElement.innerHTML = 'Error: ' + resp.statusText;
+        errorMessageElement.classList.remove('hidden');
+        errorMessageElement.classList.add('error-message');
+    } else {
+        const data = await resp.json();
+        console.log("Success:", data);
+    }
+});
